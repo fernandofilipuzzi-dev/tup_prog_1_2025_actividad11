416c88821fbc32dec7fefcceb95bacf71e780576
fernandofilipuzzi-dev <fernandofilipuzzi.dev@gmail.com>
2025-06-06 16:14:17 -0300

    ejemplo 4

--- Actividad11/Ejemplo4/FormPrincipal.Designer.cs
+++ Actividad11/Ejemplo4/FormPrincipal.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace Ejemplo3
+﻿namespace Ejemplo4
 {
     partial class FormPrincipal
     {
@@ -35,78 +35,112 @@
             lbResultado = new Label();
             btnRegistrar = new Button();
             tbValor = new TextBox();
+            tbBuscar = new TextBox();
+            btnBuscar = new Button();
+            btnListarOrdenado = new Button();
             SuspendLayout();
             // 
             // btnCalcularPromedio
             // 
-            btnCalcularPromedio.Location = new Point(302, 71);
+            btnCalcularPromedio.Location = new Point(306, 71);
             btnCalcularPromedio.Name = "btnCalcularPromedio";
             btnCalcularPromedio.Size = new Size(112, 55);
-            btnCalcularPromedio.TabIndex = 13;
+            btnCalcularPromedio.TabIndex = 20;
             btnCalcularPromedio.Text = "Calcular promedio";
             btnCalcularPromedio.UseVisualStyleBackColor = true;
             btnCalcularPromedio.Click += btnCalcularPromedio_Click;
             // 
             // tbResultado
             // 
-            tbResultado.Location = new Point(10, 132);
+            tbResultado.Location = new Point(22, 198);
             tbResultado.Multiline = true;
             tbResultado.Name = "tbResultado";
-            tbResultado.Size = new Size(339, 89);
-            tbResultado.TabIndex = 12;
+            tbResultado.Size = new Size(306, 89);
+            tbResultado.TabIndex = 19;
             tbResultado.Text = "tbResultado";
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(18, 71);
+            label3.Location = new Point(22, 71);
             label3.Name = "label3";
             label3.Size = new Size(81, 21);
-            label3.TabIndex = 11;
+            label3.TabIndex = 18;
             label3.Text = "Promedio:";
             // 
             // Valor
             // 
             Valor.AutoSize = true;
-            Valor.Location = new Point(53, 24);
+            Valor.Location = new Point(57, 24);
             Valor.Name = "Valor";
             Valor.Size = new Size(49, 21);
-            Valor.TabIndex = 10;
+            Valor.TabIndex = 17;
             Valor.Text = "Valor:";
             // 
             // lbResultado
             // 
             lbResultado.AutoSize = true;
             lbResultado.BackColor = SystemColors.ActiveCaption;
-            lbResultado.Location = new Point(115, 71);
+            lbResultado.Location = new Point(119, 71);
             lbResultado.Name = "lbResultado";
             lbResultado.Size = new Size(92, 21);
-            lbResultado.TabIndex = 9;
+            lbResultado.TabIndex = 16;
             lbResultado.Text = "lbResultado";
             // 
             // btnRegistrar
             // 
-            btnRegistrar.Location = new Point(302, 12);
+            btnRegistrar.Location = new Point(306, 12);
             btnRegistrar.Name = "btnRegistrar";
             btnRegistrar.Size = new Size(112, 52);
-            btnRegistrar.TabIndex = 8;
+            btnRegistrar.TabIndex = 15;
             btnRegistrar.Text = "Registrar";
             btnRegistrar.UseVisualStyleBackColor = true;
             btnRegistrar.Click += btnRegistrar_Click;
             // 
             // tbValor
             // 
-            tbValor.Location = new Point(115, 21);
+            tbValor.Location = new Point(119, 21);
             tbValor.Name = "tbValor";
             tbValor.Size = new Size(159, 29);
-            tbValor.TabIndex = 7;
+            tbValor.TabIndex = 14;
             tbValor.Text = "tbValor";
+            // 
+            // tbBuscar
+            // 
+            tbBuscar.Location = new Point(22, 149);
+            tbBuscar.Name = "tbBuscar";
+            tbBuscar.Size = new Size(159, 29);
+            tbBuscar.TabIndex = 21;
+            tbBuscar.Text = "tbBuscar";
+            // 
+            // btnBuscar
+            // 
+            btnBuscar.Location = new Point(199, 143);
+            btnBuscar.Name = "btnBuscar";
+            btnBuscar.Size = new Size(112, 39);
+            btnBuscar.TabIndex = 22;
+            btnBuscar.Text = "Buscar";
+            btnBuscar.UseVisualStyleBackColor = true;
+            btnBuscar.Click += btnBuscar_Click;
+            // 
+            // btnListarOrdenado
+            // 
+            btnListarOrdenado.Location = new Point(334, 209);
+            btnListarOrdenado.Name = "btnListarOrdenado";
+            btnListarOrdenado.Size = new Size(93, 54);
+            btnListarOrdenado.TabIndex = 23;
+            btnListarOrdenado.Text = "Listado Ordenado";
+            btnListarOrdenado.UseVisualStyleBackColor = true;
+            btnListarOrdenado.Click += btnListarOrdenado_Click;
             // 
             // FormPrincipal
             // 
             AutoScaleDimensions = new SizeF(9F, 21F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(421, 231);
+            ClientSize = new Size(439, 296);
+            Controls.Add(btnListarOrdenado);
+            Controls.Add(btnBuscar);
+            Controls.Add(tbBuscar);
             Controls.Add(btnCalcularPromedio);
             Controls.Add(tbResultado);
             Controls.Add(label3);
@@ -115,11 +149,9 @@
             Controls.Add(btnRegistrar);
             Controls.Add(tbValor);
             Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            FormBorderStyle = FormBorderStyle.FixedSingle;
             Margin = new Padding(4);
             Name = "FormPrincipal";
-            StartPosition = FormStartPosition.CenterScreen;
-            Text = "Ejemplo 3. Con vectores";
+            Text = "Ejemplo 4. Busqueda y Ordenamiento";
             ResumeLayout(false);
             PerformLayout();
         }
@@ -133,5 +165,8 @@
         private Label lbResultado;
         private Button btnRegistrar;
         private TextBox tbValor;
+        private TextBox tbBuscar;
+        private Button btnBuscar;
+        private Button btnListarOrdenado;
     }
 }
---
+++ Actividad11/Ejemplo4/FormPrincipal.cs
@@ -0,0 +1,105 @@
+namespace Ejemplo4
+{
+    public partial class FormPrincipal : Form
+    {
+        double[] valores = new double[100];
+        int contador = 0;
+
+        public FormPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRegistrar_Click(object sender, EventArgs e)
+        {
+            valores[contador] += Convert.ToDouble(tbValor.Text);
+            contador++;
+
+            tbValor.Clear();
+        }
+
+        private void btnCalcularPromedio_Click(object sender, EventArgs e)
+        {
+            double acumulador = 0;
+
+            for (int n = 0; n < contador; n++)
+            {
+                acumulador += valores[n];
+            }
+
+            double promedio = acumulador / contador;
+
+            lbResultado.Text = $"{promedio:f2}";
+            tbResultado.Text = $@"Promedio:
+{promedio:f2}";
+
+        }
+
+        int Buscar(int valorBuscar)
+        {
+            int idx = -1;
+            int n = 0;
+            while (n < contador && idx == -1)
+            {
+                if (valores[n] == valorBuscar)
+                {
+                    idx = n;
+                }
+                n++;
+            }
+            return idx;
+        }
+
+        int Ordenar(double[] valores, int cantidad)
+        { 
+            int idx = -1;
+            for (int pivote = 0; pivote < cantidad-1; pivote++)
+            {
+                for (int comp = pivote+1; comp < cantidad; comp++)
+                {
+                    if (valores[pivote] > valores[comp])
+                    {
+                        Intercambiar(valores, pivote, comp);
+                    }
+                }
+            }
+
+            return idx;
+        }
+
+        void Intercambiar(double[] valores, int a, int b)
+        {
+            double aux = valores[a];
+            valores[a] = valores[b];
+            valores[b] = aux;
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            int valorBuscar = Convert.ToInt32(tbBuscar.Text);
+            int idx = Buscar(valorBuscar);
+
+            if (idx > -1)
+            {
+                tbResultado.Text = $"Valor encontrado en la posición {idx}.";
+            }
+            else
+            {
+                tbResultado.Text = "Valor no buscado";
+            }
+        }
+
+        private void btnListarOrdenado_Click(object sender, EventArgs e)
+        {
+
+            Ordenar(valores, contador);
+                        
+            tbResultado.Clear();
+            for (int n = 0; n < contador; n++)
+            {
+                tbResultado.Text += $"{valores[n],5:f2}";
+            }
+
+        }
+    }
+}
